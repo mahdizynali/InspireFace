5d6102080b822afb2717dec3aba3729a0743b4a0
Mahdi Zeinali <zeinali.mahdy@gmail.com>
2025-11-05 13:18:55 +0330

    Use thread-local storage for UUID generation
    Changed random number generation to use thread-local storage for better concurrency.

--- cpp/inspireface/feature_hub/embedding_db/embedding_db.cpp
+++ cpp/inspireface/feature_hub/embedding_db/embedding_db.cpp
@@ -9,10 +9,10 @@
 #endif
 
 static std::string GenerateUUID() {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<> dis(0, 15);
-    static std::uniform_int_distribution<> dis2(8, 11);
+    thread_local std::random_device rd;
+    thread_local std::mt19937 gen(rd());
+    thread_local std::uniform_int_distribution<> dis(0, 15);
+    thread_local std::uniform_int_distribution<> dis2(8, 11);
 
     std::stringstream ss;
     ss << std::hex;
