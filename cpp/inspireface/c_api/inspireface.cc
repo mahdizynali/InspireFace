f45d8d1eab380ea77d548e595f38919641c12380
Mahdi Zeinali <m.zeinali@arad.co.net>
2025-11-05 12:31:05 +0330

    [#Feature] develop database, add uuid & name in tables

--- cpp/inspireface/c_api/inspireface.cc
+++ cpp/inspireface/c_api/inspireface.cc
@@ -1106,7 +1106,7 @@ HResult HFFeatureHubInsertFeature(HFFaceFeatureIdentity featureIdentity, HPFaceI
     for (int i = 0; i < featureIdentity.feature->size; ++i) {
         feat.push_back(featureIdentity.feature->data[i]);
     }
-    HInt32 ret = INSPIREFACE_FEATURE_HUB->FaceFeatureInsert(feat, featureIdentity.id, *allocId);
+    HInt32 ret = INSPIREFACE_FEATURE_HUB->FaceFeatureInsert(feat, featureIdentity.id, *allocId, "Target");
 
     return ret;
 }
